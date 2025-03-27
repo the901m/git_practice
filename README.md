6e9507eaa092a350e12f6b01bacbb293ffe6a86e
mmd <mmdh@example.com>
2025-03-27 16:41:56 +0330

    making some changes in temp-branch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## this is a temporary repository for test purposes
+## This is a temporary repository for test purposes
+
+simple python code to print some numbers.
+with bonus .txt file contains nothing special.
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-# my main file
-print("test")
+for i in range(126):
+    if i % 25 == 0:
+        print("this is a number: " + str(i))
+
+for i in range(0, 126, 25):
+    print("this is the same numbers with better method: " + str(i))
